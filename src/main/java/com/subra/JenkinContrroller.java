9c4c6df7240f17d31b9e4a700480f627a68370da
sdass <sdass@drf.com>
2020-05-26 21:10:49 -0400

    war file deploy to tomcat 8 through Jenkins

---
+++ src/main/java/com/subra/JenkinContrroller.java
@@ -0,0 +1,14 @@
+package com.subra;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinContrroller {
+
+	@GetMapping("/greet")
+		String sayHi(){
+			System.out.println("in sayHi()...");
+			return "Test Jenkins works!";
+		}
+}
---
+++ src/main/java/com/subra/SpringwarJenkinApplication.java
@@ -0,0 +1,13 @@
+package com.subra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwarJenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwarJenkinApplication.class, args);
+	}
+
+}
